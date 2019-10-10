6deb5eb3e4367639f851bc64a04f909b356f141c
KrupoderovMikhail <mishka.crash@gmail.com>
2019-10-10 18:59:10 +0500

    Initial commit

---
+++ src/main/java/com/krupoderov/github/untitledforum/UntitledForumApplication.java
@@ -0,0 +1,13 @@
+package com.krupoderov.github.untitledforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntitledForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krupoderov/github/untitledforum/UntitledForumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krupoderov.github.untitledforum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UntitledForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
